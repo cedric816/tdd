90db271e3d68d2131a9909712ade156a88a5cae1
cedric816 <cedricnoisette9@gmail.com>
2021-08-26 15:09:16 +0200

    fizzbuzz test 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,7 @@
 const fizzbuzz = (num) => {
-    if (num % 5 == 0 && num % 7 == 0){
+    if (num == null){
+        return "Error! need a number :)";
+    } else if (num % 5 == 0 && num % 7 == 0){
         return "fizzbuzz";
     } else if (num % 5 == 0) {
         return "buzz";
