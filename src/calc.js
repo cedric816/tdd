44bbcadadd874b039ae3157a2d141f2f8219499a
cedric816 <cedricnoisette9@gmail.com>
2021-08-26 15:51:37 +0200

    mini refactor

--- src/calc.js
+++ src/calc.js
@@ -1,24 +1,25 @@
 const calc = (nb1, nb2, operator) => {
-    if (!['+','-','*','/'].includes(operator)){
+    if (!['+', '-', '*', '/'].includes(operator)) {
         return "invalid operator";
     }
-    if (typeof nb1 !== 'number' || typeof nb2 !== 'number'){
+    if (typeof nb1 !== 'number' || typeof nb2 !== 'number') {
         return "please give numbers";
     }
-    if (operator == '+'){
-        return nb1+nb2;
+    if (operator == '+') {
+        return nb1 + nb2;
     }
-    if (operator == '-'){
-        return nb1-nb2;
+    if (operator == '-') {
+        return nb1 - nb2;
     }
-    if (operator == '/' && nb2 != 0){
-        return nb1/nb2;
+    if (operator == '/') {
+        if (nb2 !== 0) {
+            return nb1 / nb2;
+        } else {
+            return "division by 0 impossible";
+        }
     }
-    if (operator == '/' && nb2 == 0){
-        return "division by 0 impossible";
-    }
-    if (operator == '*'){
-        return nb1*nb2;
+    if (operator == '*') {
+        return nb1 * nb2;
     }
 }
 
